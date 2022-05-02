db93f44dba44b59b1f9b558bd111cdcd2121b106
Lucie.HJ.Roh <92660068+Lucie-HJ-ROH@users.noreply.github.com>
2022-05-02 13:43:41 -0400

    Init Github for JohnAbbott Project
    JohnAbbott FullStackDeveloper Course

---
+++ JohnAbbott/Session02/May02/PrintExcercise/src/May02/TestPrint.java
@@ -0,0 +1,14 @@
+package May02;
+
+public class TestPrint {
+    public static void main(String[] args) {
+        System.out.println("Test01");
+
+        System.out.println("I write this code \nin class !!!");
+
+        System.out.println("The sum of 2 and 3 = " + (2+3));
+        int result = 2 + 3;
+        System.out.println("The sum of 2 and 3 = " + result);
+    }
+}
+
